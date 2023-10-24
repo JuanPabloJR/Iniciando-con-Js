2575b3f146c6dcff93016fabd94825f40e3fde70
JuanPabloJR <jjimenez57@ucol.mx>
2023-10-23 20:34:21 -0600

    Segunda version

--- index.js
+++ index.js
@@ -2,13 +2,13 @@
 let edad;
 let nombre;
 let apellido;
-edad= prompt("Introduce tu edad",0);
-nombre= prompt("Introduce tu nombre");
-apellido= prompt("Introduce tu apellido");
-if (edad) {edad >= 18
+edad = prompt("Introduce tu edad",0);
+nombre = prompt("Introduce tu nombre");
+apellido = prompt("Introduce tu apellido");
+if (edad >=18) {
     document.write("Hola " + nombre + " " + apellido);
-    
-} else {edad <= 17
+
+} else { edad < 17
     document.write("¡Lo siento no eres mayor de edad!");
-    
-}
+
+} 
